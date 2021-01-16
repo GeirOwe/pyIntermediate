4c4bd0df2fc8cbceb5512729e5a8a757ecdba471
GeirOwe <geir.owe@hotmail.com>
2021-01-16 17:09:48 +0100

    dataframe snip
    dataframe snip

---
+++ myDataFrame.py
@@ -0,0 +1,34 @@
+# practicing dataframe w pandas and numpy
+
+import pandas as pd
+import numpy as np
+import os
+
+# ----------------------------------------
+#start of the main part of the programme
+# ----------------------------------------
+os.system('clear')
+print('\n ------- wake up gary owen ------- \n')
+
+#use pandas to read the csv file into a DataFrame
+polls = pd.read_csv('data/covid_concern_polls_adjusted.csv', low_memory=False)
+pollsagg = polls[['very', 'somewhat', 'not_very', 'not_at_all']].agg('mean')
+print('how concerned are americans about covid-19?\n') 
+print(pollsagg)
+print('\ndeltakere i spørreundersøkelsen: ', int(polls['samplesize'].sum()), '\n')
+#confPolls = polls[polls['very'] > 40]
+#print(confPolls.head())
+#print()
+#for i, v in snitt.items():
+#    print(i, float("{:.2f}".format(v)))
+
+print('\n ------- over til vin og TOPPLISTEN -------\n')
+#over til vin
+vin = pd.read_csv('data/vinkjeller.csv', sep=';', encoding = 'utf8')
+#and replace 'nan' with zero or space
+#vin['privatscore'] = vin['privatscore'].fillna(0)
+#print(vin.head())
+#       subset some rows
+drikkNu = vin[vin['bottles'] > 0]
+drikkNu = drikkNu.sort_values(['score', 'privatscore'], ascending=[False, False])
+print(drikkNu)
