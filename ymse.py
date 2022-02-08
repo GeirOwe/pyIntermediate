afc475bbebda2ec8a1bee533e34dcec1da020c54
GeirOwe <geir.owe@hotmail.com>
2022-02-08 20:00:11 +0100

    added some oo

--- ymse.py
+++ ymse.py
@@ -1,5 +1,6 @@
 #test loops
 import os
+import csv
 
 #clear the console and start the programme
 def empty():
@@ -20,7 +21,7 @@ def list_comp(y):
     sq = [x**2 for x in range(y)]
     return sq 
 
-def do_main():    
+def do_loops():    
     #no of iterations
     iter = 10
     #normal loop
@@ -32,7 +33,99 @@ def do_main():
     print('')
     return
 
+class Car():
+    def __init__(self, brand, vintage, price):
+        self.brand = brand
+        self.vintage = vintage
+        self.price = price
+        return
+
+    def get_brand(self):
+        return self.brand
+
+    def get_vintage(self):
+        return self.vintage
+
+    def get_price(self):
+        return self.price
+    
+    def set_mileage(self, mileage):
+        self.mileage = mileage
+        return 
+    
+    def get_mileage(self):
+        return self.mileage
+
+#create Car object and add to list of Car objects
+def create_car(brand, vintage, price, carObjList):
+    carObj = Car(brand, vintage, price)
+    carObjList.append(carObj)
+    return carObjList
+
+def store_data(carObjList):
+    filename = 'cars.csv'
+    header = ['Merke', 'Årgang', 'Pris', 'Kmstand']
+    #data = [['Alex', 62, 80], ['Brad', 45, 56], ['Joey', 85, 98]]
+    #build datarows
+    data = []
+    i = 0
+    while i < len(carObjList):
+        datarow = []
+        datarow.append(carObjList[i].get_brand())
+        datarow.append(carObjList[i].get_vintage())
+        datarow.append(carObjList[i].get_price())
+        datarow.append(carObjList[i].get_mileage())
+        data.append(datarow)
+        i += 1
+
+    #store data in a csv file
+    with open(filename, 'w', newline="") as file:
+        csvwriter = csv.writer(file) # 2. create a csvwriter object
+        csvwriter.writerow(header) # 4. write the header
+        csvwriter.writerows(data) # 5. write the rest of the data
+
+    return
+
+def do_objects():
+    #keep all Car objects in a List
+    carObjList = []
+    #ceate new Car objects
+    carObjList = create_car('macan', '2017', '660000', carObjList)
+    carObjList = create_car('i3', '2018', '300000', carObjList)
+    
+    #get / set some info on Car objects
+    carObjList[1].set_mileage('38000')
+    carObjList[0].set_mileage('75000')
+    print('pris ', carObjList[0].get_brand(), ': ', carObjList[0].get_price())
+    print('km ', carObjList[1].get_brand(), ': ', carObjList[1].get_mileage())
+
+    #store the data
+    store_data(carObjList)
+    return
+
+def read_cars_from_file():
+    file = open("cars.csv")
+    csvreader = csv.reader(file)
+    header = next(csvreader)
+    #print(header)
+    #add cars to a list of Car objects
+    carObjList = []
+    for row in csvreader:
+        carObjList = create_car(row[0],row[1], row[2], carObjList)
+    file.close()
+    
+    #get / set some info on Car objects
+    carObjList[1].set_mileage('38000')
+    carObjList[0].set_mileage('75000')
+    print('pris ', carObjList[0].get_brand(), ': ', carObjList[0].get_price())
+    print('km ', carObjList[1].get_brand(), ': ', carObjList[1].get_mileage())
+    print()
+
+    return
+
 #let's start
 if __name__ == '__main__':
     empty()
-    do_main()
+    #do_loops()
+    #do_objects()
+    read_cars_from_file()
